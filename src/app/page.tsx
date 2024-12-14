d4992913cbe607edca70cf78d48384d43b22a52a
kauly <kauly@hotmail.com>
2024-12-14 15:38:14 -0300

    feat: octokit setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { octokit } from "@/lib/github";
 import { Button } from "@nextui-org/button";
 
-export default function Home() {
+export default async function Home() {
+  const data = await octokit.rest.users.getByUsername({
+    username: "dalgrande",
+  });
+
+  console.log(data);
+
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <Button>Click me</Button>
